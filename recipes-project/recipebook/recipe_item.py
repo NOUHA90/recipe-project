7851e898244a60251904adc9ca9e021478122c06
NOUHA90 <39261574+NOUHA90@users.noreply.github.com>
2021-08-04 23:29:37 +0200

    Nouha: this is my second commit

--- recipes-project/recipebook/recipe_item.py
+++ recipes-project/recipebook/recipe_item.py
@@ -1,8 +1,6 @@
-class RecipeItem:
-    def __init__(self, ingredient_id, recipe_id, quantity, unit):
-        self.ingredient_id = ingredient_id
-        self.recipe_id = recipe_id
-        self.quantity = quantity
-        self.unit = unit
+from data import Data
+class RecipeItem(Data):
+    def __init__(self):
+        super.__init__()
 
         
